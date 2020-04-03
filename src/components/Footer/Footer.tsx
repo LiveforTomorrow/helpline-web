2f59707d847d9751e4ea2b9321bca996b732e228
Tataihono Nikora <tataihono.nikora@gmail.com>
2020-04-03 21:56:42 +1300

    add container

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement } from 'react';
-import { Typography, Link, Box } from '@material-ui/core';
+import { Typography, Link, Container, Box } from '@material-ui/core';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import NextLink from 'next/link';
 
@@ -7,7 +7,6 @@ const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         box: {
             textAlign: 'center',
-            margin: theme.spacing(2),
         },
         links: {
             display: 'grid',
@@ -31,29 +30,35 @@ const Footer = (): ReactElement => {
     const classes = useStyles();
 
     return (
-        <Box className={classes.box}>
-            <Typography className={classes.links}>
-                <NextLink href="/privacy" passHref>
-                    <Link className={classes.link} data-testid="privacy">
-                        Your Privacy
+        <Container maxWidth="xs">
+            <Box my={2} className={classes.box}>
+                <Typography className={classes.links}>
+                    <NextLink href="/privacy" passHref>
+                        <Link className={classes.link} data-testid="privacy">
+                            Your Privacy
+                        </Link>
+                    </NextLink>
+                    <NextLink href="/about" passHref>
+                        <Link className={classes.link} data-testid="about">
+                            About
+                        </Link>
+                    </NextLink>
+                    <Link href="mailto:elliot@livefortomorrow.co" className={classes.link} data-testid="contact">
+                        Contact
                     </Link>
-                </NextLink>
-                <NextLink href="/about" passHref>
-                    <Link className={classes.link} data-testid="about">
-                        About
+                    <Link
+                        href="https://zealnz.typeform.com/to/BtdlLP"
+                        className={classes.link}
+                        data-testid="crowdsource"
+                    >
+                        Crowdsource
                     </Link>
-                </NextLink>
-                <Link href="mailto:elliot@livefortomorrow.co" className={classes.link} data-testid="contact">
-                    Contact
-                </Link>
-                <Link href="https://zealnz.typeform.com/to/BtdlLP" className={classes.link} data-testid="crowdsource">
-                    Crowdsource
-                </Link>
-            </Typography>
-            <Typography className={classes.copyright}>
-                Powered by Live For Tomorrow &copy; {new Date().getFullYear()}
-            </Typography>
-        </Box>
+                </Typography>
+                <Typography className={classes.copyright}>
+                    Powered by Live For Tomorrow &copy; {new Date().getFullYear()}
+                </Typography>
+            </Box>
+        </Container>
     );
 };
 
