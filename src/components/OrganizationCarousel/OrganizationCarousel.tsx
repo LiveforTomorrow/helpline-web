4fed43553c064c0bf0c03ded3516dbe3025000b5
Kay Bennett <kaybennett@macbook-pro.home>
2020-04-21 20:00:54 +0100

    removed useWindowSize

--- src/components/OrganizationCarousel/OrganizationCarousel.tsx
+++ src/components/OrganizationCarousel/OrganizationCarousel.tsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect, useCallback, useContext, ReactElement, ReactNode } from 'react';
 import EmblaCarouselReact from 'embla-carousel-react';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-import { Box } from '@material-ui/core';
-import useWindowSize from '../../util/useWindowSize';
+import { Box, Container } from '@material-ui/core';
+import { useWindowResize, useThrottledFn } from 'beautiful-react-hooks';
 import ConditionalWrapper from '../../util/conditionalWrapper';
 import OrganizationContext from '../../context/organizationContext';
 import OrganizationCard from '../OrganizationCard/OrganizationCard';
@@ -13,28 +13,88 @@ type size = {
     width?: number;
 };
 
-// const organizations = [
-//     {
-//         slug: 'youthline',
-//         name: 'Youthline',
-//         alwaysOpen: true,
-//         smsNumber: '234',
-//         phoneNumber: '0800 376 633',
-//         url: 'https://www.youthline.co.nz',
-//         chatUrl: 'https://www.youthline.co.nz/web-chat-counselling.html',
-//         timezone: 'Pacific/Auckland',
-//         topics: [{ name: 'Topic 1' }],
-//         categories: [{ name: 'Category 1' }],
-//         humanSupportTypes: [],
-//         openingHours: [
-//             {
-//                 day: 'monday',
-//                 open: '2000-01-01T00:00:00Z',
-//                 close: '2000-01-01T23:59:00Z',
-//             },
-//         ],
-//     },
-// ];
+const organizations = [
+    {
+        slug: 'youthline',
+        name: 'Youthline',
+        alwaysOpen: true,
+        smsNumber: '234',
+        phoneNumber: '0800 376 633',
+        url: 'https://www.youthline.co.nz',
+        chatUrl: 'https://www.youthline.co.nz/web-chat-counselling.html',
+        timezone: 'Pacific/Auckland',
+        topics: [{ name: 'Topic 1' }],
+        categories: [{ name: 'Category 1' }],
+        humanSupportTypes: [],
+        openingHours: [
+            {
+                day: 'monday',
+                open: '2000-01-01T00:00:00Z',
+                close: '2000-01-01T23:59:00Z',
+            },
+        ],
+    },
+    {
+        slug: 'youthline',
+        name: 'Youthline',
+        alwaysOpen: true,
+        smsNumber: '234',
+        phoneNumber: '0800 376 633',
+        url: 'https://www.youthline.co.nz',
+        chatUrl: 'https://www.youthline.co.nz/web-chat-counselling.html',
+        timezone: 'Pacific/Auckland',
+        topics: [{ name: 'Topic 1' }],
+        categories: [{ name: 'Category 1' }],
+        humanSupportTypes: [],
+        openingHours: [
+            {
+                day: 'monday',
+                open: '2000-01-01T00:00:00Z',
+                close: '2000-01-01T23:59:00Z',
+            },
+        ],
+    },
+    {
+        slug: 'youthline',
+        name: 'Youthline',
+        alwaysOpen: true,
+        smsNumber: '234',
+        phoneNumber: '0800 376 633',
+        url: 'https://www.youthline.co.nz',
+        chatUrl: 'https://www.youthline.co.nz/web-chat-counselling.html',
+        timezone: 'Pacific/Auckland',
+        topics: [{ name: 'Topic 1' }],
+        categories: [{ name: 'Category 1' }],
+        humanSupportTypes: [],
+        openingHours: [
+            {
+                day: 'monday',
+                open: '2000-01-01T00:00:00Z',
+                close: '2000-01-01T23:59:00Z',
+            },
+        ],
+    },
+    {
+        slug: 'youthline',
+        name: 'Youthline',
+        alwaysOpen: true,
+        smsNumber: '234',
+        phoneNumber: '0800 376 633',
+        url: 'https://www.youthline.co.nz',
+        chatUrl: 'https://www.youthline.co.nz/web-chat-counselling.html',
+        timezone: 'Pacific/Auckland',
+        topics: [{ name: 'Topic 1' }],
+        categories: [{ name: 'Category 1' }],
+        humanSupportTypes: [],
+        openingHours: [
+            {
+                day: 'monday',
+                open: '2000-01-01T00:00:00Z',
+                close: '2000-01-01T23:59:00Z',
+            },
+        ],
+    },
+];
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -51,30 +111,39 @@ const useStyles = makeStyles((theme: Theme) =>
 
 const OrganizationCarousel = (): ReactElement => {
     const classes = useStyles();
-    const size = useWindowSize();
-    const { organizations } = useContext(OrganizationContext);
 
+    // const { organizations } = useContext(OrganizationContext);
     const [embla, setEmbla] = useState(null);
+
     const scrollPrev = useCallback(() => embla.scrollPrev(), [embla]);
     const scrollNext = useCallback(() => embla.scrollNext(), [embla]);
     const [prevBtnEnabled, setPrevBtnEnabled] = useState(false);
     const [nextBtnEnabled, setNextBtnEnabled] = useState(false);
 
+    const [width, setWidth] = useState(window.innerWidth);
+
+    useWindowResize(() => {
+        setWidth(window.innerWidth);
+    });
+
     useEffect(() => {
-        const onSelect = (): void => {
-            setPrevBtnEnabled(embla.canScrollPrev());
-            setNextBtnEnabled(embla.canScrollNext());
-        };
         if (embla) {
-            embla.on('select', onSelect);
-            onSelect();
+            // const onSelect = (): void => {
+            //     setPrevBtnEnabled(embla.canScrollPrev());
+            //     setNextBtnEnabled(embla.canScrollNext());
+            // };
+            // embla.on('select', () => {
+            //     console.log(`Current index is ${embla.selectedScrollSnap()}`);
+            // });
+            // embla.on('select', onSelect);
+            // onSelect();
         }
     }, [embla]);
 
     return (
-        <div>
+        <Container>
             <ConditionalWrapper
-                condition={size && size.width >= 320}
+                condition={true}
                 wrapper={(children): ReactElement => (
                     <EmblaCarouselReact
                         emblaRef={setEmbla}
@@ -84,18 +153,18 @@ const OrganizationCarousel = (): ReactElement => {
                     </EmblaCarouselReact>
                 )}
             >
-                <Box>
+                <EmblaCarouselReact emblaRef={setEmbla} options={{ loop: false, align: 'start', containScroll: true }}>
                     {organizations &&
                         organizations.map((organization) => (
                             <Box key={organization.slug} p={2}>
                                 <OrganizationCard organization={organization} />
                             </Box>
                         ))}
-                </Box>
+                </EmblaCarouselReact>
             </ConditionalWrapper>
             {prevBtnEnabled && <PrevButton onClick={scrollPrev} enabled={prevBtnEnabled} />}
             {nextBtnEnabled && <NextButton onClick={scrollNext} enabled={nextBtnEnabled} />}
-        </div>
+        </Container>
     );
 };
 
--- src/util/useWindowSize.test.ts
+++
@@ -1,9 +0,0 @@
-import { renderHook } from '@testing-library/react-hooks';
-import useWindowSize from './useWindowSize';
-
-test('should return new size of window', () => {
-    const { result } = renderHook(() => useWindowSize());
-
-    expect(result.current.width).toBe(1024);
-    expect(result.current.height).toBe(768);
-});
