2557577c0dc3b18260505b067e7369a3900490f8
Tataihono Nikora <tataihono.nikora@gmail.com>
2020-05-06 17:35:57 +1200

    add pageview tracking

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,14 @@
-import React, { ReactElement } from 'react';
-import App from 'next/app';
+import React, { ReactElement, useEffect } from 'react';
 import Head from 'next/head';
 import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TagManager from 'react-gtm-module';
+import { AppProps } from 'next/app';
+import { Router } from 'next/router';
 import theme from '../src/theme';
 
-export default class MyApp extends App {
-    componentDidMount(): void {
+const App = ({ Component, pageProps }: AppProps): ReactElement => {
+    useEffect(() => {
         TagManager.initialize({
             gtmId: process.env.GTM_ID,
         });
@@ -15,25 +16,26 @@ export default class MyApp extends App {
         if (jssStyles) {
             jssStyles.parentElement.removeChild(jssStyles);
         }
-    }
+        Router.events.on('routeChangeComplete', (url) => {
+            ga && ga('gtm1.send', { hitType: 'pageview', page: url });
+        });
+    }, []);
 
-    render(): ReactElement {
-        const { Component, pageProps } = this.props;
+    return (
+        <>
+            <Head>
+                <title>Find A Helpline</title>
+                <meta
+                    name="viewport"
+                    content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
+                />
+            </Head>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </>
+    );
+};
 
-        return (
-            <React.Fragment>
-                <Head>
-                    <title>Find A Helpline</title>
-                    <meta
-                        name="viewport"
-                        content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
-                    />
-                </Head>
-                <ThemeProvider theme={theme}>
-                    <CssBaseline />
-                    <Component {...pageProps} />
-                </ThemeProvider>
-            </React.Fragment>
-        );
-    }
-}
+export default App;
