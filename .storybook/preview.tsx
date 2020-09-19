efb6a3511f3a5a2f6645fed5ece6c1083212ed43
Tataihono Nikora <tataihono.nikora@gmail.com>
2020-09-19 18:33:00 +1200

    upgrade storybook

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
-import { configure, addParameters } from '@storybook/react';
+import { addParameters } from '@storybook/react';
 import { addDecorator } from '@storybook/react';
 import { ThemeProvider } from '@material-ui/core';
 import theme from '../src/theme';
 import stubOrganizationReviewCreate from '../tests/stubs/stubOrganizationReviewCreate';
 
 stubOrganizationReviewCreate();
-
 addDecorator(storyFn => <ThemeProvider theme={theme}>{storyFn()}</ThemeProvider>);
-
 addParameters({
   chromatic: { viewports: [320, 414, 834, 1200] },
 });
-
-// automatically import all files ending in *.stories.tsx
-configure(require.context('../src/components', true, /\.stories\.tsx?$/), module);
