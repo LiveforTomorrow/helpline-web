60462accedfc5de5b413ee049bd076de8dc5c7a3
Tataihono Nikora <tataihono.nikora@gmail.com>
2020-09-19 18:11:19 +1200

    add hotjar (#209)

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ module.exports = {
     env: {
         GA_ID: process.env.GA_ID,
         RECAPTCHA_KEY: process.env.RECAPTCHA_KEY,
+        HOTJAR_ID: 1995997,
     },
     webpack: (config) => {
         config.module.rules.push({
