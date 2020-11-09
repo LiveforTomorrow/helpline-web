9f89edbff1426b8d459f9ca9e0144ddba0f322be
Thomas Tayler <thomasisaactayler@gmail.com>
2020-11-10 12:48:28 +1300

    Update about.tsx
    Added Footer component to about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import React, { ReactElement } from 'react';
 import Chrome from '../src/components/Chrome';
 import About from '../src/components/About';
+import Footer from '../src/components/Footer';
 
 const AboutPage = (): ReactElement => {
     return (
@@ -11,6 +12,7 @@ const AboutPage = (): ReactElement => {
             </Head>
             <Chrome>
                 <About />
+                <Footer />
             </Chrome>
         </>
     );
